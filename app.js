62a86b6b028c88bcd9fc0bd6aa308e56e13b23eb
ksuares1 <kimberlyjsuares@gmail.com>
2023-02-16 23:55:05 -0500

    reverted css and classes back

---
+++ app.js
@@ -0,0 +1,14 @@
+let first = document.querySelector(".firstName");
+let last = document.querySelector(".lasttName");
+let mail = document.querySelector(".email");
+let pass = document.querySelector(".password");
+
+function information(){
+if (first == "") {
+  alert("Please enter your first name.");
+}
+
+}
+information();
+// let contactInfo=document.querySelector(".claim");
+// claim.addEventlistener("click", information);
