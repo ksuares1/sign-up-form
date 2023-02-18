13c84f8683c7a44b59e36c87b2f84b98203d8bfa
ksuares1 <kimberlyjsuares@gmail.com>
2023-02-18 12:19:03 -0500

    changed media query for min-width

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
-let form= document.getElementById('form');
-let first = document.getElementById("firstName");
-let last = document.getElementById("lasttName");
-let mail = document.getElementById("email");
-let pass = document.getElementById("password");
+const form = document.getElementById('form');
 
 
-form.addEventListener("click", e =>{
+
+form.addEventListener("submit", e =>{
   e.preventDefault();
 
-  const first = form['firstname'].value;
-  alert(firstValue);
+  const firstName = form['firstname'].value;
+  const lastName = form["lastname"].value; 
+  const email = form["email"].value;
+  const password = form["password"].value;
+
+  if(firstName === ""){
+    addErrorTo("firstname", "First Name is required");
+  }
+  if (lastName === "") {
+    addErrorTo("firstname", "Last Name is required");
+  }
+  if (email === "") {
+    addErrorTo("email", "Email is required");
+  }
+  if (password === "") {
+    addErrorTo("password", "Password is required");
+  }
+  function addErrorTo(field,message){
+    const small = form["field"].parentNode.querySelector("small");
+    small.innerText = "message";
+    small.style.opacity = "1";
+
+  }
 });
 
 
